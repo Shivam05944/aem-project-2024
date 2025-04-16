967f44f690d569c7222e03afa8ae47f3f5c500c0
Shivam05944 <87103937+Shivam05944@users.noreply.github.com>
2025-04-16 23:22:18 +0530

    Bring images from assets

--- core/src/main/java/com/myproject/core/models/SearchImage.java
+++ core/src/main/java/com/myproject/core/models/SearchImage.java
@@ -1,4 +1,7 @@
 package com.myproject.core.models;
 
+import java.util.List;
+
 public interface SearchImage {
+    List<String> getDemoImage();
 }
--- core/src/main/java/com/myproject/core/models/impl/SearchImageImpl.java
+++ core/src/main/java/com/myproject/core/models/impl/SearchImageImpl.java
@@ -1,4 +1,59 @@
 package com.myproject.core.models.impl;
 
-public class SearchImageImpl {
+import com.day.cq.dam.api.Asset;
+import com.day.cq.dam.api.Rendition;
+import com.day.cq.search.PredicateGroup;
+import com.day.cq.search.Query;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.search.result.Hit;
+import com.day.cq.search.result.SearchResult;
+import com.myproject.core.models.SearchImage;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.inject.Inject;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Model(adaptables = {SlingHttpServletRequest.class}, adapters = {SearchImage.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+
+public class SearchImageImpl implements SearchImage {
+    @ValueMapValue
+    public String damPath;
+    @Inject
+    private ResourceResolver resourceResolver;
+    @Inject
+    private QueryBuilder queryBuilder;
+
+    @Override
+    public List<String> getDemoImage() {
+        List<String> image = new ArrayList<>();
+        Session session = resourceResolver.adaptTo(Session.class);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("path", damPath);
+        map.put("type", "dam:Asset");
+        map.put("p.limit", "-1");
+
+        Query query = queryBuilder.createQuery(PredicateGroup.create(map), session);
+        SearchResult result = query.getResult();
+
+        for (Hit hit : result.getHits()) {
+            try {
+                image.add(hit.getPath());
+            } catch (RepositoryException e) {
+                e.printStackTrace();
+            }
+        }
+        return image;
+    }
 }
