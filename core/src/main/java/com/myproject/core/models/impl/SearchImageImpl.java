7e53ee613c71550b8451b7dd64ce7c53991c43d3
Shivam05944 <87103937+Shivam05944@users.noreply.github.com>
2025-04-17 13:38:16 +0530

    clientlib-searchimage

--- core/src/main/java/com/myproject/core/models/impl/SearchImageImpl.java
+++ core/src/main/java/com/myproject/core/models/impl/SearchImageImpl.java
@@ -13,7 +13,6 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import javax.inject.Inject;
